581e02218036f7c4339e3b8ec01da656868cf3c6
Omadjon2004 <122809872+Omadjon2004@users.noreply.github.com>
2023-03-09 22:40:42 +0500

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+
+
+
+const years=[1985,1995,2000,2004,1950]
+
+const calcBirth=function(x){
+    
+ return ` Siz ${2023-x} yoshdasiz`
+}
+
+for (let i = 0; i < years.length; i++) {
+    console.log(calcBirth(years[i]));
+}
+
+
+
